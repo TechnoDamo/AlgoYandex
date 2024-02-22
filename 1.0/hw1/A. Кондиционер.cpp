0d1b48f59fcd2fb6ccd45550be6aeb9808cc699d
Damir Koblev <101816707+TechnoDamo@users.noreply.github.com>
2024-02-22 17:38:41 +0300

    Update A. Кондиционер.cpp

--- 1.0/hw1/A. Кондиционер.cpp
+++ 1.0/hw1/A. Кондиционер.cpp
@@ -1,1 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int tRoom, tDes;
+    string s;
+    cin >> tRoom >> tDes;
+    cin >> s;
+    int ans;
+    if (tRoom == tDes) {
+        ans = tRoom;
+    }
+    else if (tRoom > tDes) {
+        if (s == "auto" || s == "freeze") ans = tDes;
+        else ans = tRoom;
+    }
+    else {
+        if (s == "auto" || s == "heat") ans = tDes;
+        else ans = tRoom;
+    }
+    cout << ans;
+    return 0;
+}
